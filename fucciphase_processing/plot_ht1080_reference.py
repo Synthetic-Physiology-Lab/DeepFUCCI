213fa01e59d9ea157c15285680f831a52a77866d
Julius Zimmermann <juliusmanfredkurt.zimmermann@unipv.it>
2025-05-07 19:14:17 +0200

    plot for Fig. 3 updated

--- fucciphase_processing/plot_ht1080_reference.py
+++ fucciphase_processing/plot_ht1080_reference.py
@@ -4,6 +4,6 @@ from fucciphase.plot import plot_raw_intensities
 
 ref_data = pd.read_csv("ht1080_reference.csv")
 
-plot_raw_intensities(ref_data, channel1="cyan", channel2="magenta", time_column="time", time_label="Time / h") 
+plot_raw_intensities(ref_data, channel1="cyan", channel2="magenta", time_column="percentage", time_label="Cell cycle percentage", lw=4) 
 plt.savefig("reference_ht1080.pdf")
 plt.show()
