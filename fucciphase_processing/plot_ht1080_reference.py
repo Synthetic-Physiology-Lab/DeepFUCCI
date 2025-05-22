bae5cae7016aa3986756a5cf4a55d488347762e4
Julius Zimmermann <juliusmanfredkurt.zimmermann@unipv.it>
2025-05-22 19:29:17 +0200

    more plot scripts

--- fucciphase_processing/plot_ht1080_reference.py
+++ fucciphase_processing/plot_ht1080_reference.py
@@ -1,9 +1,41 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from fucciphase.plot import plot_raw_intensities
 
-ref_data = pd.read_csv("ht1080_reference.csv")
+ref_data = pd.read_csv("fuccisa_ht1080_reference.csv")
 
-plot_raw_intensities(ref_data, channel1="cyan", channel2="magenta", time_column="percentage", time_label="Cell cycle percentage", lw=4) 
+channel1="cyan"
+channel2="magenta"
+time_column="time"
+percentage_column="percentage"
+time_label="Time / h"
+percentage_label="Cell cycle percentage"
+color1 = "cyan"
+color2 = "magenta"
+
+
+ch1_intensity = ref_data[channel1]
+ch2_intensity = ref_data[channel2]
+t = ref_data[time_column]
+perc = ref_data[percentage_column]
+
+fig, ax1 = plt.subplots()
+
+# prepare axes
+ax1.set_xlabel(time_label)
+ax1.set_ylabel("mTurquoise2")
+ax1.tick_params(axis="y", labelcolor=color1)
+ax2 = ax1.twinx()
+ax2.set_ylabel("miRFP670")
+ax2.tick_params(axis="y", labelcolor=color2)
+
+ax3 = ax1.twiny()
+
+# plot signal
+ax1.plot(t, ch1_intensity, color=color1, lw=4)
+ax2.plot(t, ch2_intensity, color=color2, lw=4)
+ax3.plot(perc, ch1_intensity, color=color1, lw=4)
+ax3.set_xlabel(percentage_label)
+
+fig.tight_layout()
 plt.savefig("reference_ht1080.pdf")
 plt.show()
